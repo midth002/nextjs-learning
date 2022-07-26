613523127f125d3ae633d37bde53705a7d4afd91
Andrew Midthun <midth002@gmail.com>
2022-07-26 11:46:59 -0500

    Added useRouter to use params. Why next.js is so awesome.

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div>about</div>
+    <div><h2>This is the about page.</h2></div>
   )
 }
 
---
+++ pages/profile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileDefault = () => {
+  return (
+    <div>Profile Default</div>
+  )
+}
+
+export default ProfileDefault
